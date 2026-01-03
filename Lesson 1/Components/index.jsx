74f3de9543f352561a8859bf9fdc1d6a74c1a0b2
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2026-01-03 09:27:38 +0530

    Create index.jsx

---
+++ Lesson 1/Components/index.jsx
@@ -0,0 +1,46 @@
+import {StyleSheet,Image, View, Text } from 'react-native'
+import React from 'react'
+import Logo from "../assets/icon.png"
+
+
+const Home = () => {
+  return (
+    <View style={styles.container} >
+        <Image source={Logo} style={styles.img}/>
+      <Text style={styles.title}>The Number 1</Text>
+
+      <Text style={{fontWeight:500,margin:10}}>Reading List App</Text>
+
+
+
+    </View>
+  )
+}
+
+export default Home
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  title:{
+    fontWeight:"bold",
+    fontSize:18,
+  },
+  card:{
+   
+    backgroundColor:"#000",
+    padding:2,
+    borderRadius:5,
+    boxShadow:"0px 0px 2px #0f0",  
+  },
+  img:{
+    marginVertical:10,
+    height:200,
+    width:200,
+    borderRadius:18,
+  }
+})
+
